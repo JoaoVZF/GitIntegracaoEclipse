12271262e78b249f91bd84be090c865d64103a5b
JoaoVZF <jvzfbito@gmail.com>
2025-07-24 16:04:37 -0300

    Primeiro commit

---
+++ ProjetoIntegracaoGitEclipseGitHub/src/TesteIntegraca.java
@@ -0,0 +1,10 @@
+
+public class TesteIntegraca {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Teste");
+	}
+
+}
+	
---
+++ ProjetoIntegracaoGitEclipseGitHub/src/TesteIntegracao.java
@@ -0,0 +1,8 @@
+
+public class TesteIntegracao {
+
+	public static void main(String[] args) {
+	 System.out.println("Primeiro teste");
+	}
+
+}
