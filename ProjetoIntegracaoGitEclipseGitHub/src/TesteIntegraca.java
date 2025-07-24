1f937765a1dd8fce7bcdf4462575193011846f38
JoaoVZF <jvzfbito@gmail.com>
2025-07-24 16:17:50 -0300

    segundo commit

--- ProjetoIntegracaoGitEclipseGitHub/src/TesteIntegraca.java
+++ ProjetoIntegracaoGitEclipseGitHub/src/TesteIntegraca.java
@@ -4,7 +4,10 @@ public class TesteIntegraca {
 	public static void main(String[] args) {
 		
 		System.out.println("Teste");
+		System.out.println("Teste 2");
 	}
+	
+	
 
 }
 	
--- ProjetoIntegracaoGitEclipseGitHub/src/TesteIntegracao.java
+++ ProjetoIntegracaoGitEclipseGitHub/src/TesteIntegracao.java
@@ -3,6 +3,7 @@ public class TesteIntegracao {
 
 	public static void main(String[] args) {
 	 System.out.println("Primeiro teste");
+	 System.out.println("Segundo teste");
 	}
 
 }
